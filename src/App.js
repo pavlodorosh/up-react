a0048e437204888bf7c0379a0fa5e4db361dba97
pavlodorosh <dpm1987@gmail.com>
2023-08-12 16:57:03 +0300

    life component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import Comp1 from './Comp1';
-import Comp2 from './Comp2';
-
-const App = () => {
-  return (
-    <>
-    <Comp1/>
-    <Comp2/>
-    </>
-  );
-};
-
+// import Test1 from './Test1';
+// import Test2 from './Test2';
+import CompInputs from './CompInputs';
+function App(){
+    return(
+        <>
+        {/* <Test2 arg={107}/>
+        <Test1 arg={107}/> */}
+        <CompInputs/>
+        </>
+    )
+}
 export default App;
